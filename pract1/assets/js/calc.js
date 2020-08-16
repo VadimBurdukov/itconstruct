14f60299814fddda73fb6a2d62ad8fca7199d70c
Vadimburdukov <PM71.Burdukov@outlook.com>
2020-08-16 17:43:03 +0700

    js, a little of css

--- pract1/assets/js/calc.js
+++ pract1/assets/js/calc.js
@@ -1,11 +1,22 @@
 $(document).ready(function () {
+
+    var inputBox = document.querySelector(".count");
+    var invalidChars = [
+        "-",
+        "+",
+        "e",
+    ];
+    inputBox.addEventListener("keydown", function (e) {
+        if (invalidChars.includes(e.key)) {
+            e.preventDefault();
+        }
+    });
     $('a.counter__change').on('click', function (e) {
-        
-        var oper = $(this).attr("oper");
+        var oper = $(this).attr("data-oper");
         var count = $(this).closest('.counter').find('input');
             switch (oper) {
                 case '-':
-                    if (count.val() != 1) {
+                    if (count.val() > 1) {
                         count.val(count.val() - 1);
                     }
                     break;
