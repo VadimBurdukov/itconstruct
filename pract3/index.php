bdadf632b11159cca5fea64d60322fdc81443544
Vadimburdukov <PM71.Burdukov@outlook.com>
2020-10-21 18:15:28 +0700

    pract3

---
+++ pract3/index.php
@@ -0,0 +1,17 @@
+<?
+$url = "https://api.github.com/users/vadimburdukov";
+
+$cInit = curl_init();
+
+curl_setopt($cInit, CURLOPT_URL, $url);
+curl_setopt($cInit, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($cInit, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+
+$output = curl_exec($cInit);
+$info = curl_getinfo($cInit, CURLINFO_HTTP_CODE);
+$result = json_decode($output);
+
+curl_close($cInit);
+
+echo '<pre>'.print_r($result, true).'</pre>';
+?>
